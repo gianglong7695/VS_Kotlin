05569618a3cdcbe6475036239464a7a727f195e5
Giang Long <gianglong7695@gmail.com>
2018-12-26 08:32:20 +0700

    sync

--- app/src/main/java/com/vtv/sports/view/adapter/VideoAdapter.kt
+++ app/src/main/java/com/vtv/sports/view/adapter/VideoAdapter.kt
@@ -7,7 +7,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.vtv.sports.R
-import com.vtv.sports.databinding.ItemVideoBoxBinding
+import com.vtv.sports.databinding.ItemVideoBinding
+import com.vtv.sports.databinding.ItemVideoHeaderBinding
 import com.vtv.sports.model.video.VideoGroup
 import com.vtv.sports.util.TimeDateUtils
 
@@ -16,7 +17,11 @@ import com.vtv.sports.util.TimeDateUtils
  * Skype: gianglong7695@gmail.com (id: gianglong7695_1)
  * Des:
  */
-class VideoAdapter(c: Context) : RecyclerView.Adapter<VideoAdapter.VideoVH>() {
+class VideoAdapter(c: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    val TYPE_HEADER = 1
+    val TYPE_ITEM = 2
+    val TYPE_FOOTER = 3
+
     val inflater: LayoutInflater = LayoutInflater.from(c)
     var listVideo: List<VideoGroup> = listOf()
 
@@ -25,69 +30,65 @@ class VideoAdapter(c: Context) : RecyclerView.Adapter<VideoAdapter.VideoVH>() {
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): VideoVH {
-        var binding: ItemVideoBoxBinding =
-            DataBindingUtil.inflate(inflater, R.layout.item_video_box, viewGroup, false)
-        return VideoVH(binding)
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == TYPE_HEADER) {
+            var binding: ItemVideoHeaderBinding =
+                DataBindingUtil.inflate(inflater, R.layout.item_video_header, viewGroup, false)
+            return HeaderVH(binding)
+        } else {
+            var binding: ItemVideoBinding =
+                DataBindingUtil.inflate(inflater, R.layout.item_video, viewGroup, false)
+            return ItemVH(binding)
+        }
     }
 
     override fun getItemCount(): Int {
-        return listVideo.size
+        return 3
     }
 
-    override fun onBindViewHolder(holder: VideoVH, pos: Int) {
-        holder.setData(listVideo[pos])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, pos: Int) {
+        if (holder is HeaderVH)
+            holder.setData(listVideo[0])
+//        if (holder is ItemVH)
+//            holder.setData(listVideo[pos],)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0)
+            return TYPE_HEADER
+        return TYPE_ITEM
     }
 
 
-    inner class VideoVH(binding: ItemVideoBoxBinding) : RecyclerView.ViewHolder(binding.root) {
-        val binding: ItemVideoBoxBinding = binding
-//        var itemAdapter: VideoItemAdapter
-//        lateinit var listItemVideo: List<VideoBox>
-
-
-        init {
-//            binding.recyclerVideo.layoutManager =
-//                    GridLayoutManager(binding.root.context, 2, GridLayoutManager.VERTICAL, false)
-//            binding.recyclerVideo.setHasFixedSize(true)
-//            binding.recyclerVideo.isNestedScrollingEnabled = false
-//            itemAdapter = VideoItemAdapter(binding.root.context)
-//            binding.recyclerVideo.adapter = itemAdapter
-        }
+    inner class HeaderVH(binding: ItemVideoHeaderBinding) : RecyclerView.ViewHolder(binding.root) {
+        val binding: ItemVideoHeaderBinding = binding
 
         fun setData(video: VideoGroup) {
             val first = video.listVideo[0]
             binding.textCategory.text = video.videoZone.name
-
             Glide.with(binding.root.context).load(first.avatar).into(binding.imgAvatar)
             binding.textTitle.text = first.name
             binding.textDate.text = TimeDateUtils.getScheduleSport(first.distributionDate)
             binding.textTime.text = TimeDateUtils.convertToDate(first.distributionDate)
             binding.textDuration.text = TimeDateUtils.convertToDuration(first.duration)
-
-            //Item first
-            Glide.with(binding.root.context).load(video.listVideo[1].avatar).into(binding.itemFirst.imgAvatar)
-            binding.itemFirst.textTitle.text = video.listVideo[1].name
-            binding.itemFirst.textTime.text = TimeDateUtils.convertToDate(video.listVideo[1].distributionDate)
-
-            //Item second
-            Glide.with(binding.root.context).load(video.listVideo[2].avatar).into(binding.itemSecond.imgAvatar)
-            binding.itemSecond.textTitle.text = video.listVideo[2].name
-            binding.itemSecond.textTime.text = TimeDateUtils.convertToDate(video.listVideo[2].distributionDate)
-
-            //Item third
-            Glide.with(binding.root.context).load(video.listVideo[3].avatar).into(binding.itemThird.imgAvatar)
-            binding.itemThird.textTitle.text = video.listVideo[3].name
-            binding.itemThird.textTime.text = TimeDateUtils.convertToDate(video.listVideo[3].distributionDate)
-
-            //Item fourth
-            Glide.with(binding.root.context).load(video.listVideo[4].avatar).into(binding.itemFourth.imgAvatar)
-            binding.itemFourth.textTitle.text = video.listVideo[4].name
-            binding.itemFourth.textTime.text = TimeDateUtils.convertToDate(video.listVideo[4].distributionDate)
+        }
+    }
 
 
-//            listItemVideo = video.listVideo.subList(1, 5)
-//            itemAdapter.setVideoList(listItemVideo)
+    class ItemVH(binding: ItemVideoBinding) : RecyclerView.ViewHolder(binding.root) {
+        val binding: ItemVideoBinding = binding
+
+
+        fun setData(video: VideoGroup, pos: Int) {
+            val first = video.listVideo[pos]
+            val second = video.listVideo[2]
+//            binding.textCategory.text = video.videoZone.name
+//            Glide.with(binding.root.context).load(first.avatar).into(binding.imgAvatar)
+//            binding.textTitle.text = first.name
+//            binding.textDate.text = TimeDateUtils.getScheduleSport(first.distributionDate)
+//            binding.textTime.text = TimeDateUtils.convertToDate(first.distributionDate)
+//            binding.textDuration.text = TimeDateUtils.convertToDuration(first.duration)
         }
     }
 
